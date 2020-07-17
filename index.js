52e1c6e3834a9fbed29feb2b02d5125e3b330d42
ihollander <ianholla@gmail.com>
2020-07-17 15:45:09 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,75 @@
+require('dotenv/config');
+
+const express = require('express');
+const app = express();
+
+const fetch = require('node-fetch');
+
+const getAuthed = (code) => {
+  const url = `https://zoom.us/oauth/token?grant_type=authorization_code&code=${code}&redirect_uri=${process.env.redirectURL}`;
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      Authorization: `Basic ${Buffer.from(
+        process.env.clientID + ':' + process.env.clientSecret
+      ).toString('base64')}`,
+    },
+  }).then((r) => r.json());
+};
+
+// there's a limit of requesting a 1 month range for recordings
+// so it will take several requests to get everything
+const getRecordings = (month, token) => {
+  const startMonth = month.toString().padStart(2, '0');
+  const endMonth = (month + 1).toString().padStart(2, '0');
+  return fetch(
+    `https://api.zoom.us/v2/users/me/recordings?page_size=300&from=2020-${startMonth}-01&to=2020-${endMonth}-01`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  ).then((r) => r.json());
+};
+
+// hook up oauth
+app.get('/zoom', async (req, res) => {
+  // Step 1:
+  // Check if the code parameter is in the url
+  // if an authorization code is available, the user has most likely been redirected from Zoom OAuth
+  // if not, the user needs to be redirected to Zoom OAuth to authorize
+  console.log('startMonth', req.query.startMonth);
+  if (req.query.code) {
+    // Step 3:
+    // Request an access token using the auth code
+    const authBody = await getAuthed(req.query.code);
+
+    if (authBody.access_token) {
+      let meetings = [];
+      let month = 3; // TODO: use queryString to set month range
+      let endMonth = 7;
+      // Step 4:
+      // We can now use the access token to authenticate API calls
+      try {
+        while (month <= endMonth) {
+          console.log('fetching month', month);
+          const recordings = await getRecordings(month, authBody.access_token);
+          meetings.push(...recordings.meetings);
+          month++;
+        }
+      } catch (err) {
+        console.log(err);
+        return res.send(err.message);
+      }
+      res.json(meetings);
+    }
+    return;
+  }
+
+  // If no authorization code is available, redirect to Zoom OAuth to authorize
+  res.redirect(
+    `https://zoom.us/oauth/authorize?response_type=code&client_id=${process.env.clientID}&redirect_uri=${process.env.redirectURL}`
+  );
+});
+
+app.listen(4000, () => console.log(`Zoom app listening at PORT: 4000`));
